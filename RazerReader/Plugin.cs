642cd90f5813b83187374bc4b9c6b01af02f1cc4
Mashiro-chan <11561006+mashirochan@users.noreply.github.com>
2025-03-30 00:09:59 -0400

    Update to API 12

--- RazerReader/Plugin.cs
+++ RazerReader/Plugin.cs
@@ -20,10 +20,7 @@ namespace RazerReader;
 public sealed class Plugin : IDalamudPlugin
 {
     [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
-    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
-    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
     [PluginService] internal static IFramework Framework { get; private set; } = null!;
     [PluginService] internal static IDtrBar DtrBar { get; private set; } = null!;
